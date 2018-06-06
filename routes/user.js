cb181016429d0a653eaf96668267996d5c281396
iphayao <phayaob@gmail.com>
2018-06-06 22:49:37 +0700

    Add simple RESTful api

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.json({user: 'hello world'})
+});
+
+router.get('/:id', function(req, res) {
+    res.json(req.params)
+});
+
+router.post('/', function(req, res) {
+    res.json({message: 'add user complated'})
+});
+
+router.patch('/:id', function(req, res) {
+    res.json({message: `user ${req.params.id} modified`})
+});
+
+router.delete('/:id', function(req, res) {
+    res.json({message: `user ${req.params.id} deleted`})
 });
 
 module.exports = router;
