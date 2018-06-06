79f0d6471a2c06d7f9117dc35280c4eaa587c723
iphayao <phayaob@gmail.com>
2018-06-06 22:30:29 +0700

    Add simple routing

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const indexRoute = require('./routes/index');
+const userRoute = require('./routes/user');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', indexRoute);
+app.use('/users', userRoute);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 'use strict';
 
-const express = require('express');
+//const express = require('express');
 
 // constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // app
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello world\n');
-});
+const app = require('./app');
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
